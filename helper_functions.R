655c72ab83133a616ab7de550548fc290ada9433
lukasbirki <73613044+lukasbirki@users.noreply.github.com>
2021-06-22 19:34:31 +0200

    Add files via upload

---
+++ helper_functions.R
@@ -0,0 +1,19 @@
+# Reverse words
+reverse_words_helper <- function(string)
+{
+  # split string by blank spaces
+  string_split = strsplit(as.character(string), split = "-")
+  # how many split terms?
+  string_length = length(string_split[[1]])
+  # decide what to do
+  if (string_length == 1) {
+    # one word (do nothing)
+    reversed_string = string_split[[1]]
+  } else {
+    # more than one word (collapse them)
+    reversed_split = string_split[[1]][string_length:1]
+    reversed_string = paste(reversed_split, collapse = "-")
+  }
+  # output
+  return(reversed_string)
+} 
